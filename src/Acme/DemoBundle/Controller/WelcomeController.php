9231ec4545bf93a204bcc9c2f593ecaecba80259
aweqqas <hellal.riad@gmail.com>
2014-12-15 12:33:50 +0100

    sdfs sdfsdf

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -3,17 +3,36 @@
 namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class WelcomeController extends Controller
 {
+    /**
+     * @Route("/", name="index_classified")
+     * @Template()
+     */
     public function indexAction()
     {
-        /*
-         * The action's view can be rendered using render() method
-         * or @Template annotation as demonstrated in DemoController.
-         *
-         */
-
-        return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+        return array();
     }
+    
+    /**
+     * @Route("/login", name="login_classified")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        return array();
+    }    
+    
+    /**
+     * @Route("/result", name="result_classified")
+     * @Template()
+     */
+    public function resultAction()
+    {
+        return array();
+    }        
 }
