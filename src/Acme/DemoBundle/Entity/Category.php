0000dbb0cb13a2b3e42a6b7d2c41304378daa00a
aweqqas <hellal.riad@gmail.com>
2014-12-17 17:15:34 +0100

    sdfsdf sdfsdf

---
+++ src/Acme/DemoBundle/Entity/Category.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Acme\DemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="Category", indexes={@ORM\Index(name="cat_parent_idx", columns={"parent"})})
+ * @ORM\Entity
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="category_order", type="integer", nullable=false)
+     */
+    private $categoryOrder;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="level", type="integer", nullable=false)
+     */
+    private $level;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=255, nullable=false)
+     */
+    private $slug;
+
+    /**
+     * @var \Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="parent", referencedColumnName="id")
+     * })
+     */
+    private $parent;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set categoryOrder
+     *
+     * @param integer $categoryOrder
+     * @return Category
+     */
+    public function setCategoryOrder($categoryOrder)
+    {
+        $this->categoryOrder = $categoryOrder;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryOrder
+     *
+     * @return integer 
+     */
+    public function getCategoryOrder()
+    {
+        return $this->categoryOrder;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     * @return Category
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer 
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Category
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \Acme\DemoBundle\Entity\Category $parent
+     * @return Category
+     */
+    public function setParent(\Acme\DemoBundle\Entity\Category $parent = null)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \Acme\DemoBundle\Entity\Category 
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+}
